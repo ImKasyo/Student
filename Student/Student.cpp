806bba1a4c971c7d2b5350b8e3eb919b1585f69c
Mandi_pv66 <Mandi_pv66@student.itstep.org>
2024-09-18 08:17:32 +0300

    Создал класс Student. Обьявил поля класса

--- Student/Student.cpp
+++ Student/Student.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
+class Student {
+private:
+	char* fullName;
+	char* dateOfBirth;
+	char* phoneNumber;
+	char* institution;
+	char* group;
+};
+
 int main(){
 
 }
