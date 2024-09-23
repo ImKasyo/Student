37f03450033810f6fc0bef5df6467d1e137914ee
Mandi_pv66 <Mandi_pv66@student.itstep.org>
2024-09-23 06:07:31 +0300

    Добавил констурктор с параметрами

--- Student/Student.cpp
+++ Student/Student.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<Windows.h>
 using namespace std;
 
 class Student {
@@ -12,9 +13,15 @@ public:
 	Student() {
 		fullName, dateOfBirth, phoneNumber, institution, group = nullptr;
 	}
+	Student(const char * name, const char * dateOfBirth, const char * phoneNumber, ) {
+		fullName = new char[strlen(fullName) +1];
+		strcpy_s(fullName, strlen(fullName) + 1, name);
+	}
 	
 };
-
+// Реализуйте функции-члены класса для ввода данных, вывода данных, реализуйте
+// аксессоры для доступа к отдельным переменным-членам.
+// Добавьте необходимые конструкторы, деструктор.
 int main(){
 
 }
